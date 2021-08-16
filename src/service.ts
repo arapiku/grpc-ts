450bff2193cb6d92de0d0b502c68460ac3756182
arapiku <arapiku420@gmail.com>
2021-08-16 14:47:09 +0900

    modify: args type

--- src/service.ts
+++ src/service.ts
@@ -1,8 +1,8 @@
-import { ServerUnaryCall } from 'grpc'
+import { ServerUnaryCall, ServiceError } from 'grpc'
 import { UsersReply, UsersRequest } from './protos/service_pb'
 import { findUsers } from './repository'
 
-export const getUsers = async (call: ServerUnaryCall<UsersRequest>, callback: any) => {
+export const getUsers = async (call: ServerUnaryCall<UsersRequest>, callback: (error: ServiceError | null, response: UsersReply) => void) => {
   const request = call.request
   const reply = new UsersReply()
   reply.setUsersList(await findUsers(request.getResultcount()))
