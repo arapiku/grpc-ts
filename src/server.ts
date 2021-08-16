466c0f98321a23fc1a2603d5663a5ab242b56e2c
arapiku <arapiku420@gmail.com>
2021-08-16 12:55:44 +0900

    feat: impl server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
-const sample = (message: string) => {
-  console.log(message)
-}
+import { Server, ServerCredentials } from "grpc"
+import { UserDomainService } from "./protos/service_grpc_pb"
+import { getUsers } from './service'
+
+const grpcServer = new Server()
+grpcServer.addService(UserDomainService, { getUsers })
+grpcServer.bind('localhost:50051', ServerCredentials.createInsecure())
+grpcServer.start()
